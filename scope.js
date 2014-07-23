c364fe1e870e2024e6085acb1b27344961ccd2ee
nitesh sharma <nitesh.grep@gmail.com>
2014-07-23 11:31:19 +0530

    refactored scope class

--- scope.js
+++ scope.js
@@ -1,21 +1,37 @@
-var Scope = function(){
-	this.$$watchCollection = [];
-};
+var Scope = util.defClass({
+	
+	constructor: function() {
+		this.$$watchCollection = [];
+	},
+	
+	$watch: function(watchFn, listenFn) {
+		var $watchobj = {
+			watchFn: watchFn,
+			listenFn: listenFn
+		};
 
-Scope.prototype.$watch = function(watchFn, listenFn){
-	var $watchobj = {
-		watchFn: watchFn,
-		listenFn: listenFn
-	};
+		this.$$watchCollection.push($watchobj);
+	},
+	
+	$digest: function() {
+		this.$$watchCollection.forEach(function($watch) {
+			$watch.listenFn();
+		});
+	}
 
-	this.$$watchCollection.push($watchobj);
-};
+});
+
+
 
 var scope = new Scope();
 
-watchFn = function(){ console.log("watch");};
-listenFn = function(){ condole.log("listen");};
+watchFn = function() {
+	console.log("watch");
+};
+listenFn = function() {
+	console.log("listen");
+};
 
 scope.$watch(watchFn, listenFn);
-
+scope.$digest();
 console.log(scope);
