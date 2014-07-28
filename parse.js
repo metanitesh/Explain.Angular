7411303ea4789f377b59b9f22e17e436f1b4a08a
nitesh sharma <nitesh.grep@gmail.com>
2014-07-28 14:03:47 +0530

    adding floating point parsing

--- parse.js
+++ parse.js
@@ -1,46 +1,56 @@
 var Lexer = util.defClass({
-	constructor: function(){
-
+	constructor: function() {
+		this.text = undefined;
+		this.index = 0;
+		this.ch = undefined;
+		this.tokens = [];
 	},
 
-	isNumber : function(ch){
+	isNumber: function(ch) {
 		return '0' <= ch && ch <= '9';
 	},
 
-	readNumber: function(){
+	peek: function() {
+		if (this.index < this.text.length - 1) {
+			return this.text[index + 1];
+		} else {
+			return false;
+		}
+
+	},
+	readNumber: function() {
 		var number = "";
-		
-		while(this.index < this.text.length){
+
+		while (this.index < this.text.length) {
 
 			var ch = this.text[this.index];
-			if(this.isNumber(ch)){
+			if (this.isNumber(ch) || ch === '.') {
 				number += ch;
-			}else{
+			} else {
 				break;
 			}
 			this.index++;
 		}
 
-		number = 1*number;
-		
+		number = 1 * number;
+
 		this.tokens.push({
 			text: number,
-			fn: _.constant(number)
+			fn: _.constant(number),
+			json: true
 		});
 	},
 
-	lex: function(str){
-		this.text = str;
-		this.index = 0;
-		this.ch = undefined;
-		this.tokens = [];
+	lex: function(str) {
 
-		while(this.index<this.text.length){
-			
+		this.text = str;
+		while (this.index < this.text.length) {
+
 			this.ch = this.text[this.index];
-			if(this.isNumber(this.ch)){
+
+			if (this.isNumber(this.ch)) {
 				this.readNumber();
-			}else{
+			} else {
 				throw "woo";
 			}
 		}
@@ -51,18 +61,31 @@ var Lexer = util.defClass({
 
 
 var Parser = util.defClass({
-	constructor: function(lexer){
+
+	constructor: function(lexer) {
 		this.lexer = lexer;
+		this.tokens = [];
 	},
 
-	parse: function(exp){
-		return this.lexer.lex(exp);
+	primary: function() {
+		var token = this.tokens[0];
+		var primary = token.fn;
+		if (token.json) {
+			primary.constant = true;
+			primary.literal = true;
+		}
+		return primary;
+	},
+
+	parse: function(exp) {
+		this.tokens = this.lexer.lex(exp);
+		return this.primary();
 	}
 });
 
 
 
-function parse (exp){
+function parse(exp) {
 	var lexer = new Lexer();
 	var parser = new Parser(lexer);
 	return parser.parse(exp);
@@ -70,6 +93,4 @@ function parse (exp){
 
 
 
-console.log(parse("1122")[0].fn());
-
- 
+console.log(parse("0.2232")());
