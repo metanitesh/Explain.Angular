7a5b64b6a93be951ba91ad6af81c4c8cf5eb782d
nitesh sharma <nitesh.grep@gmail.com>
2014-07-28 11:09:50 +0530

    adding basic skeleton for parser and lexer

--- parse.js
+++ parse.js
@@ -1,3 +1,30 @@
-function parse(expr){
-	
+function parse (exp){
+	var lexer = new Lexer();
+	var parser = new Parser(lexer);
+	return parser.parse(exp);
 }
+
+var Lexer = util.defClass({
+	constructor: function(){
+
+	},
+
+	lex: function(str){
+		return str;
+	}
+});
+
+
+var Parser = util.defClass({
+	constructor: function(lexer){
+		this.lexer = lexer;
+	},
+
+	parse: function(exp){
+		this.token = this.lexer.lex(exp);
+		return this.token;
+	}
+});
+
+console.log(parse("42"));
+
