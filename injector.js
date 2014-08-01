f4f400726240943901d99ae038278e963fdf265c
nitesh sharma <nitesh.grep@gmail.com>
2014-08-01 13:02:40 +0530

    adding has and get method on injector

--- injector.js
+++ injector.js
@@ -1,4 +1,29 @@
 function createInjector(modulesToLoad){
-	return {};
+	
+	var cache = {};
+
+	var $provide = {
+		constant : function(key, val){
+			cache[key] = value;
+		}
+	};
+
+	_.forEach(modulesToLoad, function(moduleName){
+		var module = angular.module(moduleName);
+		_.forEach(module._invokeQueue, function(invokeArgs){
+			var method = invokeArgs[0];
+			var args = invokeArgs[1];
+			$provide[method].apply($provide, args);
+		});
+	});
+
+	return {
+		has: function(key){
+			return cache.hasOwnProperty(key);
+		},
+		get: function(key){
+			return cache[key];
+		}
+	};
 }
 
