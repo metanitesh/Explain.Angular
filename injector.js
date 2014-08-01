62c691b93d415c89f569ab0d3edb1a1770826f77
nitesh sharma <nitesh.grep@gmail.com>
2014-08-01 18:23:54 +0530

    adding simple invoke feature in injector

--- injector.js
+++ injector.js
@@ -1,30 +1,64 @@
-function createInjector(modulesToLoad){
-	
+function createInjector(modulesToLoad) {
+
 	var cache = {};
+	loadedModules = {};
 
 	var $provide = {
-		constant : function(key, val){
-			cache[key] = value;
+		constant: function(key, val) {
+			cache[key] = val;
 		}
 	};
 
-	_.forEach(modulesToLoad, function loadModule(moduleName){
-		var module = angular.module(moduleName);
-		_.forEach(module.requires, loadModule);
-		_.forEach(module._invokeQueue, function(invokeArgs){
-			var method = invokeArgs[0];
-			var args = invokeArgs[1];
-			$provide[method].apply($provide, args);
+	function invoke(fn, self) {
+		
+		var args = _.map(fn.$inject, function(token) {
+			return cache[token];
 		});
+
+		return fn.apply(self, args);
+	}
+
+	_.forEach(modulesToLoad, function loadModule(moduleName) {
+		if (!loadedModules.hasOwnProperty(moduleName)) {
+
+			loadedModules[moduleName] = true;
+			var module = angular.module(moduleName);
+			_.forEach(module.requires, loadModule);
+			_.forEach(module._invokeQueue, function(invokeArgs) {
+				var method = invokeArgs[0];
+				var args = invokeArgs[1];
+				$provide[method].apply($provide, args);
+			});
+
+		}
 	});
 
+	console.log(cache);
+	console.log(loadedModules);
+
 	return {
-		has: function(key){
+		has: function(key) {
 			return cache.hasOwnProperty(key);
 		},
-		get: function(key){
+		get: function(key) {
 			return cache[key];
-		}
+		},
+		invoke: invoke
 	};
 }
 
+angular.module("myApp", []);
+console.log(angular.module("myApp"));
+
+angular.module("myApp").constant("a", 1);
+angular.module("myApp").constant("b", 2);
+angular.module("myApp").constant("name", "nitesh");
+
+var fn = function(a, b) {
+	return a + b;
+};
+
+fn.$inject = ["name", "a"];
+var inject = createInjector(["myApp"]);
+
+console.log(inject.invoke(fn));
