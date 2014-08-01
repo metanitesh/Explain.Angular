a6da2658cffb70a927d2d79d2d9e8d5a38c31251
nitesh sharma <nitesh.grep@gmail.com>
2014-08-01 11:43:28 +0530

    adding angular module creation logic

--- loader.js
+++ loader.js
@@ -1,6 +1,45 @@
 function setupModuleLoader(window){
-	var angular = (window.angular = window.angular || {});
+
+	var ensure = function(obj, name, factory){
+		return obj[name] || ( obj[name] = factory());
+	};
+		
+	var angular = ensure(window, "angular", Object);
 	
+	var createModule = function(name, requires, modules){
+
+		var moduleInstance = {
+			name : name,
+			requires: requires
+		};
+
+		modules[name] = moduleInstance;
+
+		return moduleInstance;
+	};
+
+	var getModule = function(name, modules){
+
+		if(modules.hasOwnProperty(name)){
+			return modules[name];
+		}else{
+			throw "module " +name+ " is not available ";
+		}
+	};
+
+	ensure(angular, 'module', function(){
+		var modules = {};
+
+		return function(name, requires){
+			if(requires){
+				return createModule (name, requires, modules);
+			} else {
+				return getModule(name, modules);
+			}
+			
+		};
+	});
 }
 
 setupModuleLoader(window);
+	
