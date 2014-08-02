1ce77a19d57ac2aec80df0a8767485386d8f0365
nitesh sharma <nitesh.grep@gmail.com>
2014-08-02 12:04:19 +0530

    adding provider in module loader

--- loader.js
+++ loader.js
@@ -1,45 +1,51 @@
-function setupModuleLoader(window){
+function setupModuleLoader(window) {
 
-	var ensure = function(obj, name, factory){
-		return obj[name] || ( obj[name] = factory());
+	var ensure = function(obj, name, factory) {
+		return obj[name] || (obj[name] = factory());
 	};
-		
+
 	var angular = ensure(window, "angular", Object);
-	
-	var createModule = function(name, requires, modules){
+
+	var createModule = function(name, requires, modules) {
+
+		var invokeLater = function(method) {
+			return function() {
+				moduleInstance._invokeQueue.push([method, arguments]);
+			};
+		};
 
 		var moduleInstance = {
-			name : name,
-			requires: requires
+			name: name,
+			requires: requires,
+			constant: invokeLater('constant'),
+			provider: invoakeLater('provider'),
+			_invokeQueue: []
 		};
 
 		modules[name] = moduleInstance;
-
 		return moduleInstance;
 	};
 
-	var getModule = function(name, modules){
-
-		if(modules.hasOwnProperty(name)){
+	var getModule = function(name, modules) {
+		if (modules.hasOwnProperty(name)) {
 			return modules[name];
-		}else{
-			throw "module " +name+ " is not available ";
+		} else {
+			throw "module " + name + " is not available ";
 		}
 	};
 
-	ensure(angular, 'module', function(){
+	ensure(angular, 'module', function() {
 		var modules = {};
 
-		return function(name, requires){
-			if(requires){
-				return createModule (name, requires, modules);
+		return function(name, requires) {
+			if (requires) {
+				return createModule(name, requires, modules);
 			} else {
 				return getModule(name, modules);
 			}
-			
+
 		};
 	});
 }
 
 setupModuleLoader(window);
-	
