4e01902390f82ee3a0a6138884b3f1a2b56b345f
nitesh sharma <nitesh.grep@gmail.com>
2014-08-15 21:41:30 +0530

    compiling dom in directive

--- compile.js
+++ compile.js
@@ -4,12 +4,51 @@ function $CompileProvider($provide) {
 	hasDirectives = [];
 
 	this.$get = function() {
-		return "helloD";
+
+		function compile($nodes) {
+			return compileNodes($nodes);
+		}
+
+
+		function compileNodes($nodes) {
+			_.forEach($nodes, function(node) {
+				var directives = collectDirectives(node);
+				applyDirectivesToNode(directives, node);
+			});
+		}
+
+		function collectDirectives(node) {
+			var directives = [];
+			var normalizedNodeName = _.camelCase(nodeName(node).toLowerCase());
+			addDirective(directives, normalizedNodeName);
+			return directives;
+		}
+
+		function camelCase(name) {
+			return name.replace(/([\:\-\_]+(.))/g,
+				function(match, separator, letter, offset) {
+					return offset > 0 ? letter.toUpperCase() : letter;
+				});
+		}
+
+		function nodeName(element) {
+			return element.nodeName ? element.nodeName : element[0].nodeName;
+		}
+
+		function addDirective(directive, name){
+			if(hasDirectives.hasOwnProperty(name)){
+				directives.push.apply(directives, $injector.get(name + 'Directive'));
+			}
+		}
+
+		function applyDirectivesToNode(){
+			
+		}
+
+		return compile;
+
 	};
 
-	getme = function(){
-		return hasDirectives;
-	}
 
 	this.directive = function(name, directiveFactory) {
 
