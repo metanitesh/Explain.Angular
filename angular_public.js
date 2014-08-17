ad354509db9eea3bbc2215ddd3746a9bbf6d5cb0
nitesh sharma <nitesh.grep@gmail.com>
2014-08-17 18:03:16 +0530

    refactoring angular public

--- angular_public.js
+++ angular_public.js
@@ -1,12 +1,26 @@
-function publishExternalAPI(){
-	
+function publishExternalAPI() {
+
 	setupModuleLoader(window);
 
 	ngModule = angular.module('ng', []);
 	ngModule.provider('$parse', $ParseProvider);
 	ngModule.provider('$rootScope', $RootScopeProvider);
+	ngModule.provider('$compile', $CompileProvider);
 
-	inj = createInjector(['ng'])
+	ngModule.directive('myEl', function() {
+		return {
+			compile: function(element) {
+				element.data('hasCompiled', true);
+			}
+		};
+	});
+
+
+	inj = createInjector(['ng']);
+
+	inj.invoke(function($compile) {
+		$compile($("<my-el></my-el>"));
+	});
 	
 }
 
